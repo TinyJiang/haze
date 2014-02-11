9c89a5179359d07a5ae26bae428efe732398f535
jhjiangABC <jhjiang888@gmail.com>
2014-02-11 17:12:43 +0800

    first commit

---
+++ js/haze/src/main.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+			// var Spinning = require('./spinning');
+			// var s = new Spinning('#container');
+			// s.render();
+			require('jquery');
+			
+			
+			
+		});
